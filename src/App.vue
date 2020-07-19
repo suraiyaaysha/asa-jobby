ea6e3a5000231551a6086e2911a57faaad282dda
Suraiya Aysha <sm.asa15nov@gmail.com>
2020-07-20 01:26:45 +0600

    Pagination Done

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <TopHeader />
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import TopHeader from "@/components/TopHeader";
+
+export default {
+  components: {
+    TopHeader
+  }
+};
+</script>
---
+++ src/components/JobCard.vue
@@ -0,0 +1,31 @@
+<template>
+      <b-col md="3">
+        <b-card
+          :title="name"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-info"
+          img-top
+          tag="article"
+          style="max-width:20rem"
+          class="mb-4"
+          footer="Card Footer"
+        >
+          <b-card-text>
+            lorem ipsum lorem ipsum lorem ipsum bangladesh
+          </b-card-text>
+          <b-button varient="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+</template>
+
+<script>
+export default {
+  name: "JobCard",
+  props: ["name"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
